9bbc501cb3110ed2e15f510c83b560e02bcf59d0
sngraihan <raihan.sng123@gmail.com>
2025-10-07 12:43:21 +0700

    Selesai langkah praktikum modul 5

--- app/Models/MataKuliah.php
+++ app/Models/MataKuliah.php
@@ -4,11 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
-class MataKuliah extends Model
+class Matakuliah extends Model
 {
     use HasFactory;
 
     protected $table = 'mata_kuliah';
     protected $guarded = ['id'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function getAllMK()
+    {
+        return $this->all();
+    }
 }
